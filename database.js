89b9edad2190730d1adb68891499ebb9461a9a89
yushanlu <yushanlu1031@gmail.com>
2024-08-20 18:43:26 +0800

    Modify code for postgres #3

--- database.js
+++ database.js
@@ -1,51 +1,20 @@
-import sqlite3 from "sqlite3";
-const sql3 = sqlite3.verbose();
+import pkg from 'pg';
+const { Pool } = pkg;
 
-// Create a new database object, stored in memory
-const db = new sql3.Database('./data.db', sqlite3.OPEN_READWRITE, (err) => {
-  if (err) {
-    console.error('Error opening database:', err.message);
-  } else {
-    console.log('DB created or DB already exists');
-  }
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const pool = new Pool({
+    user: process.env.PGUSER,
+    host: process.env.PGHOST,
+    database: process.env.PGDATABASE,
+    password: process.env.PGPASSWORD,
+    port: process.env.PGPORT,
 });
 
-// Create a new table called "words"
-// db.run(`DROP TABLE IF EXISTS Words`, (err) => {
-//     if (err) {
-//         console.error('Error deleting existing table:', err.message);
-//         return;
-//     }else{
-//         console.error('Table deleted');
-//     }
-// })
-
-db.run(`
-CREATE TABLE IF NOT EXISTS Words (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    phrase TEXT,
-    pronounciation TEXT,
-    definition TEXT,
-    tags TEXT,
-    audioURL TEXT
-)
-`, (err) => {
-if (err) {
-    console.error('Error creating table:', err.message);
-    return;
-} else {
-    console.log('Words table created successfully.');
-}
-});
-
-    
-  
-//     stmt.finalize((err) => {
-//       if (err) {
-//         console.error('Error inserting data:', err.message);
-//       } else {
-//         console.log('Dummy data inserted successfully.');
-//       }
-//     });
-
-export {db}
+export const db = {
+    query: (text, params, callback) => {
+        return pool.query(text, params, callback);
+    },
+};
