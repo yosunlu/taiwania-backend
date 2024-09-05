7f17d47ebd6f0de1f6a077268bc153492aadcf46
yushanlu <yushanlu1031@gmail.com>
2024-09-04 21:48:00 -0500

    update database.js

--- express-backend/database.js
+++ express-backend/database.js
@@ -1,20 +1,20 @@
-import pkg from 'pg';
+import pkg from "pg";
 const { Pool } = pkg;
 
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
 
 dotenv.config();
 
 const pool = new Pool({
-    user: process.env.PGUSER,
-    host: process.env.PGHOST,
-    database: process.env.PGDATABASE,
-    password: process.env.PGPASSWORD,
-    port: process.env.PGPORT,
+  user: process.env.PGUSER,
+  host: process.env.PGHOST,
+  database: process.env.PGDATABASE,
+  password: process.env.PGPASSWORD,
+  port: process.env.PGPORT,
 });
 
 export const db = {
-    query: (text, params, callback) => {
-        return pool.query(text, params, callback);
-    },
+  query: (text, params, callback) => {
+    return pool.query(text, params, callback);
+  },
 };
